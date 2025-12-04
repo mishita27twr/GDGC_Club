cf6aad13d624722da06e27ce3ff280a68cee2cdd
Mishita0823 <mishitatiwari27@gmail.com>
2025-12-04 15:06:55 +0530

    Initial commit for backend

--- task2/index.js
+++ task2/index.js
@@ -1,3 +1,25 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// let members = [
+//   { id: 1, name: "Anubhavi Jaiswal", registrationNumber: "22BXXXXXXX", designation: "GDSC Club", role: "Lead" },
+//   { id: 2, name: "Akshay Kumar Mishra", registrationNumber: "22BXXXXXXX", designation: "GDSC Technical " , role: "Lead" },
+//   { id: 3, name: "Gagan Bhardwaj", registrationNumber: "22BXXXXXXX", designation: "GDSC Technical " , role: "Co-Lead" },];
+
+// app.get("/members", (req, res) => res.json(members));
+// app.get("/members/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   const member = members.find(m => m.id === id);
+//   if (!member) return res.status(404).json({ message: "Member not found" });
+//   res.json(member);
+// });
+
+// app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+
 const express = require("express");
 const cors = require("cors");
 
@@ -5,17 +27,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let members = [
-  { id: 1, name: "Anubhavi Jaiswal", registrationNumber: "22BXXXXXXX", designation: "GDSC Club", role: "Lead" },
+let members = [{ id: 1, name: "Anubhavi Jaiswal", registrationNumber: "22BXXXXXXX", designation: "GDSC Club", role: "Lead" },
   { id: 2, name: "Akshay Kumar Mishra", registrationNumber: "22BXXXXXXX", designation: "GDSC Technical " , role: "Lead" },
   { id: 3, name: "Gagan Bhardwaj", registrationNumber: "22BXXXXXXX", designation: "GDSC Technical " , role: "Co-Lead" },];
 
-app.get("/members", (req, res) => res.json(members));
-app.get("/members/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const member = members.find(m => m.id === id);
-  if (!member) return res.status(404).json({ message: "Member not found" });
-  res.json(member);
+app.get("/members", (req, res) => {
+  res.json(members);
 });
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
